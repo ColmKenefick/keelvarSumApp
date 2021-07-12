aa2a2c7fd18327f00df2038ab72432533a5c35bd
Colm Kenefick <colmkenefick@Colm-K-MacBook-Pro-2.local>
2021-07-12 17:00:24 +0100

    added js file - no react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
        <h3> Array of values [8, 7, 5, 4]
-      <KeelvarTechTest arg={ [8, 7, 5, 4]} />
+          <KeelvarTechTest arg={ [8, 7, 5, 4]} />
         </h3>
     </div>
   );
